c121d2c7412b94125b38320af6cd1d2247e941b6
toniette <luis_ti@outlook.com>
2023-08-19 12:03:20 -0300

    setup basic customer related files

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Authentication\AuthenticationController;
+use App\Http\Controllers\Api\Customer\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/authenticate' , [AuthenticationController::class , 'authenticate']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/customers', CustomerController::class);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
